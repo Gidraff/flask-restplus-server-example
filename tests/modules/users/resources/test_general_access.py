c97973d45863d6603a1a2caa706948368f44ce14
Gidraff <gidraff.karanja@andela.com>
2018-05-07 17:17:27 +0300

    uncomment tests

--- tests/modules/users/resources/test_general_access.py
+++ tests/modules/users/resources/test_general_access.py
@@ -3,8 +3,10 @@ import pytest
 
 
 @pytest.mark.parametrize('http_method,http_path', (
-    ('GET', '/api/v1/auth/oauth2_clients/'),
-    ('POST', '/api/v1/auth/oauth2_clients/'),
+    ('GET', '/api/v1/users/'),
+    ('GET', '/api/v1/users/1'),
+    ('PATCH', '/api/v1/users/1'),
+    ('GET', '/api/v1/users/me'),
 ))
 def test_unauthorized_access(http_method, http_path, flask_app_client):
     response = flask_app_client.open(method=http_method, path=http_path)
